bbb46d5481e1da77d912244d8c19acc30682e30f
Lavanya Chand <lavanyachand113@gmail.com>
2025-08-22 12:23:37 -0500

    minor fix

--- src/components/UI/Timer.jsx
+++ src/components/UI/Timer.jsx
@@ -47,7 +47,7 @@ const Timer = ({ expiryDate }) => {
         // When this component is removed from the screen, 
         // or when the effect runs again, 
         // stop the animation loop so it doesn’t keep running in the background.
-        // Without this cleanup, your updateTimer function could keep running endlessly and waste resources.
+        // Without this cleanup, updateTimer function could keep running endlessly and waste resources.
 
 
     }, [expiryDate])
@@ -78,74 +78,3 @@ const Timer = ({ expiryDate }) => {
 }
 
 export default Timer
-
-
-
-
-// import React, { useState, useRef, useEffect } from "react";
-
-// const CountdownTimer = ({ expiryDate }) => {
-//   // countdown: total time in milliseconds (e.g. 5 mins = 5 * 60 * 1000)
-
-//   const [timeLeft, setTimeLeft] = useState({
-//     hoursText: "00",
-//     minutesText: "00",
-//     secondsText: "00",
-//   });
-
-//   const [status, setStatus] = useState("");
-//   const requestRef = useRef(null);
-//   const startTimeRef = useRef(null);
-
-//   const countdown = new Date(expiryDate).getTime() - Date.now();
-//   const updateTimer = () => {
-//     const millisElapsed = Date.now() - startTimeRef.current;
-//     let millisLeft = countdown - millisElapsed;
-
-//     if (millisLeft <= 0) {
-//       // Clamp and mark expired
-//       setTimeLeft({ hoursText: "00", minutesText: "00", secondsText: "00" });
-//       setStatus("EXPIRED");
-
-//       cancelAnimationFrame(requestRef.current);
-//       requestRef.current = null;
-//       return; // stop updating
-//     }
-
-//     const totalSeconds = Math.floor(millisLeft / 1000);
-//     const hoursText = String(Math.floor(totalSeconds / 3600)).padStart(1, "0");
-//     const minutesText = String(Math.floor((totalSeconds % 3600) / 60)).padStart(1, "0");
-//     const secondsText = String(totalSeconds % 60).padStart(1, "0");
-
-//     setTimeLeft({ hoursText, minutesText, secondsText });
-
-//     requestRef.current = requestAnimationFrame(updateTimer);
-//   };
-
-//   useEffect(() => {
-//     startTimeRef.current = Date.now();
-//     requestRef.current = requestAnimationFrame(updateTimer);
-
-//     // Cleanup on unmount
-//     return () => {
-//       if (requestRef.current) {
-//         cancelAnimationFrame(requestRef.current);
-//       }
-//     };
-//     // eslint-disable-next-line react-hooks/exhaustive-deps
-//   }, []);
-
-//   return (
-//     <div className="de_countdown">
-//       {status === "EXPIRED" ? (
-//         <span>{status}</span>
-//       ) : (
-//         <span>
-//           {timeLeft.hoursText}h {timeLeft.minutesText}m {timeLeft.secondsText}s
-//         </span>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default CountdownTimer;
